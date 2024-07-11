253362dc30e5499ba34ab9e5d9710bb1b3e095f3
ihmpavel <42217494+ihmpavel@users.noreply.github.com>
2024-07-11 12:59:34 +0000

    refactor: Update Upload AWS provider to v5

--- packages/providers/upload-aws-s3/src/__tests__/utils.test.ts
+++ packages/providers/upload-aws-s3/src/__tests__/utils.test.ts
@@ -1,6 +1,5 @@
 import { ObjectCannedACL } from '@aws-sdk/client-s3';
 import type { InitOptions } from '..';
-import { extractCredentials } from '../utils';
 
 const accessKeyId = 'AWS_ACCESS_KEY_ID';
 const secretAccessKey = 'AWS_ACCESS_SECRET';
@@ -15,63 +14,31 @@ const defaultOptions = {
 };
 
 describe('Utils', () => {
-  describe('Extract credentials for V4 different aws provider configurations', () => {
-    test('[Legacy] Credentials directly in the options', () => {
-      const options: InitOptions = {
-        accessKeyId,
-        secretAccessKey,
-        ...defaultOptions,
-      };
-      const credentials = extractCredentials(options);
-
-      expect(credentials).toEqual({
-        accessKeyId,
-        secretAccessKey,
-      });
-    });
-
-    test('[Legacy] credentials directly in s3Options', () => {
-      const options: InitOptions = {
-        s3Options: {
+  test('Credentials in credentials object inside s3Options', () => {
+    const options: InitOptions = {
+      s3Options: {
+        credentials: {
           accessKeyId,
           secretAccessKey,
-          ...defaultOptions,
         },
-      };
-      const credentials = extractCredentials(options);
+        ...defaultOptions,
+      },
+    };
+    const credentials = options.s3Options.credentials;
 
-      expect(credentials).toEqual({
-        accessKeyId,
-        secretAccessKey,
-      });
+    expect(credentials).toEqual({
+      accessKeyId,
+      secretAccessKey,
     });
+  });
+  test('Does not throw an error when credentials are not present', () => {
+    const options: InitOptions = {
+      s3Options: {
+        ...defaultOptions,
+      },
+    };
+    const credentials = options.s3Options.credentials;
 
-    test('Credentials in credentials object inside s3Options', () => {
-      const options: InitOptions = {
-        s3Options: {
-          credentials: {
-            accessKeyId,
-            secretAccessKey,
-          },
-          ...defaultOptions,
-        },
-      };
-      const credentials = extractCredentials(options);
-
-      expect(credentials).toEqual({
-        accessKeyId,
-        secretAccessKey,
-      });
-    });
-    test('Does not throw an error when credentials are not present', () => {
-      const options: InitOptions = {
-        s3Options: {
-          ...defaultOptions,
-        },
-      };
-      const credentials = extractCredentials(options);
-
-      expect(credentials).toEqual(null);
-    });
+    expect(credentials).toEqual(null);
   });
 });
